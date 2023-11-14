88d6136b357b6d7b914b1c83bf27eb4a1719b133
Genzord111 <101523213+Genzord111@users.noreply.github.com>
2023-11-14 23:26:22 +0000

    Update main.h

--- main.h
+++ main.h
@@ -1,21 +1,13 @@
+#include <stdarg.h>
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdarg.h>
-#include <unistd.h>
-#include <limits.h>
-
-int _putchar(char character);
-int printf_char(va_list args, int printed);
-int printf_string(va_list args, int printed);
-int printf_integer(va_list args, int printed);
-int selector(const char *format, va_list args, int printed);
-int printf_binary(unsigned int num, int printed);
+int _putchar(char c);
 int _printf(const char *format, ...);
-int _x(unsigned int num, int printed, int uppercase);
-int printf_octal(unsigned int num, int printed);
-int printf_unsigned(unsigned int num, int printed);
-int printf_reverse(va_list args, int printed);
-int printf_pointer(va_list args, int printed);
-#endif /* MAIN_H */
+int format_s(va_list args, char c, int count);
+int print_number(int n, int count);
+int print_string(char *str);
+int print_binary(int decimal);
+int print_char(char c);
+int _escapechar(char n);
+#endif
