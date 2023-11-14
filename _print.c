e579cf5bf8b55d51e42d1b05dd07c9f0eba947bd
Godsway <gasamoah@wacci.ug.edu.gh>
2023-11-14 05:48:29 -0800

    Trial 5

--- _print.c
+++ _print.c
@@ -1,116 +1,88 @@
-#include <stdarg.h>
-#include <stddef.h>
-#include <string.h>
-#include <stdio.h>
 #include "main.h"
 
+void cleanup(va_list args, buffer_t *output);
+int run_printf(const char *format, va_list args, buffer_t *output);
+int _printf(const char *format, ...);
+
 /**
- * format_s - checks the type of format specifier
- * @args: list of infinite variables
- * @c: character to be checked
- * @count: tracks number of char printed
- * Return: count
+ * cleanup - Starts a  cleanup operations for printf
+ * @args: Specifies a variable list of arguments provided to printf
+ * @output: Displays a buffer_t struct
  */
-
-int format_s(va_list args, char c, int count)
+void cleanup(va_list args, buffer_t *output)
 {
-	int num_var;
-	char *str_var;
-	char char_var;
-
-	if (c == 'd')
-	{
-		num_var = va_arg(args, int);
-		count = print_number(num_var, count);
-	}
-	else if (c == 's')
-	{
-		str_var = va_arg(args, char *);
-		count += print_string(str_var);
-	}
-	else if (c == 'c')
-	{
-		char_var = va_arg(args, int);
-		count += print_char(char_var);
-	}
-	else if (c == '%')
-	{
-		_putchar(37);
-		count += 1;
-	}
-	else
-	{
-		_putchar(c);
-		count += 1;
-	}
-	return (count);
+	va_end(args);
+	write(1, output->start, output->len);
+	free_buffer(output);
 }
 
 /**
- * print_string - writes a string to stdout
- * @str: string parameter to be printed
- * Return: count
+ * run_printf - Starts the format string for printf
+ * @format: Specifies the character string to print
+ * @output: Specifies a  buffer_t struct containing a buffer
+ * @args: Specifies  variable list of arguments
+ * Return: The number of characters stored to output (Ends)
  */
-
-int print_string(char *str)
+int run_printf(const char *format, va_list args, buffer_t *output)
 {
-	int i = 0;
+	int i, wid, prec, ret = 0;
+	char tmp;
+	unsigned char flags, len;
+	unsigned int (*f)(va_list, buffer_t *,
+			unsigned char, int, int, unsigned char);
 
-	if (str != NULL)
+	for (i = 0; *(format + i); i++)
 	{
-		while (str[i] != '\0')
+		len = 0;
+		if (*(format + i) == '%')
 		{
-			_putchar(str[i]);
-			i++;
+			tmp = 0;
+			flags = handle_flags(format + i + 1, &tmp);
+			wid = handle_width(args, format + i + tmp + 1, &tmp);
+			prec = handle_precision(args, format + i + tmp + 1,
+					&tmp);
+			len = handle_length(format + i + tmp + 1, &tmp);
+
+			f = handle_specifiers(format + i + tmp + 1);
+			if (f != NULL)
+			{
+				i += tmp + 1;
+				ret += f(args, output, flags, wid, prec, len);
+				continue;
+			}
+			else if (*(format + i + tmp + 1) == '\0')
+			{
+				ret = -1;
+				break;
+			}
 		}
-		return (i);
+		ret += _memcpy(output, (format + i), 1);
+		i += (len != 0) ? 1 : 0;
 	}
-	return (0);
+	cleanup(args, output);
+	return (ret);
 }
 
 /**
- * _printf - produces output according to a format similar to printf.
- * @format: string parameter to be printed
- * Return: count
+ * _printf - Displays a formatted string
+ * @format: Specifies the character string to print
+ * Return: The number of characters printed (Ends)
  */
-
 int _printf(const char *format, ...)
 {
-	int i = 0;
-	int count = 0;
+	buffer_t *output;
 	va_list args;
+	int ret;
+
+	if (format == NULL)
+		return (-1);
+	output = init_buffer();
+	if (output == NULL)
+		return (-1);
 
 	va_start(args, format);
 
-	while (format[i] != '\0')
-	{
-		if (format[i] == '%')
-		{
-			count = format_s(args, format[i + 1], count);
-			i++;
-		}
-		else if (format[i] == '\\')
-		{
-			if (format[i + 1] == '\\')
-			{
-				_putchar(92);
-				count += 1;
-				i = i + 2;
-			}
-			else
-			{
-				count = _escapechar(format[i + 1]);
-				i++;
-			}
-		}
-		else
-		{
-			_putchar(format[i]);
-			count += 1;
-		}
-		i++;
-	}
+	ret = run_printf(format, args, output);
 
-	va_end(args);
-	return (count);
+	return (ret);
 }
