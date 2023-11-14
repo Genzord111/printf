4b89c114044dd10a627fdaed81a230873623a93f
Genzord111 <101523213+Genzord111@users.noreply.github.com>
2023-11-14 23:25:39 +0000

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,34 +1,120 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
 
-/**
- * _printf - Start execution of printf
- * @format: Specifies the format specifier
- * Return: The formated string (Ends)
- */
+int format_s(va_list args, char c, int count)
+{
+	int num_var;
+	char *str_var;
+	char char_var;
+
+
+        switch (c)
+        {
+                case 'd':
+                        num_var = va_arg(args, int);
+                        count = print_number(num_var, count);
+                        break;
+                case 's':
+                        str_var = va_arg(args, char *);
+                        count += print_string(str_var);
+                        break;
+                case 'c':
+                        char_var = va_arg(args, int);
+                        count += print_char(char_var);
+                        break;
+                case 'b':
+                        num_var = va_arg(args, int);
+                        count += print_binary(num_var);
+                        break;
+
+
+                default:
+                        _putchar(c);
+                        count += 1;
+        }
+
+
+        return count;
+}
+
+int print_number(int n, int count)
+{
+        unsigned int num;
+        num = n;
+
+        if (n < 0)
+        {
+                _putchar(45);
+                num = -num;
+                count += 1;
+        }
+        if ((num / 10) != 0)
+        {
+                count = print_number((num / 10), count);
+        }
+        _putchar((num % 10) + '0');
+
+
+        return count + 1;
+}
+
+int print_string(char *str)
+{
+        int i = 0;
+
+        if (str != NULL)
+        {
+                while (str[i] != '\0')
+                {
+                        _putchar(str[i]);
+                        i++;
+                }
+                return i;
+        }
+        return 0;
+}
+
+int print_char(char c)
+{
+        _putchar(c);
+
+        return 1;
+}
 
 int _printf(const char *format, ...)
 {
-	int printed = 0;
+        int i = 0;
+        int count = 0;
+        va_list args;
 
-	va_list args;
+        va_start(args, format);
 
-	va_start(args, format);
+        while (format[i] != '\0')
+        {
+                if (format[i] == '%')
+                {
+                        count = format_s(args, format[i+1], count);
+                        i++;
+                }
+                else if (format[i] == '\\')
+                {
+                        count = _escapechar(format[i+1]);
+                        i++;
+                }
+                else
+                {
+                        _putchar(format[i]);
+                        count += 1;
+                }
+                i++;
 
-	while (*format != '\0')
-	{
-		if (*format == '%')
-		{
-			format++;
-			printed = selector(format, args, printed);
-			format++;
-		}
-		else
-		{
-			_putchar(*format);
-			printed++;
-			format++;
-		}
-	}
-	va_end(args);
-	return (printed);
+        }
+
+        va_end(args);
+
+        return count;
 }
